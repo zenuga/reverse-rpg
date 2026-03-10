5a365653994cd0f708e60b5c3667bc8d3ed8e454
zenuga <dario.cloin@gmail.com>
2026-03-10 13:17:03 +0100

    added script to fix

---
+++ reverse rpg/Assets/DarioFolder/Code/Inventory/ItemPickup.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public ItemScript itemData;
+    
+    private void OnValidate()
+    {
+        if (itemData != null && GetComponent<Collider>() == null)
+        {
+            Debug.LogWarning("ItemPickup needs a Collider component!");
+        }
+    }
+}
