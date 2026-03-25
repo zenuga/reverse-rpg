3baf9ea54f3979ce7d5b470a0bcafa9d3be1fe0a
Nellipro <nellifrasen@gmail.com>
2026-03-25 13:52:56 +0100

    MORE SOUND

--- reverse rpg/Assets/JustinFolder/Scripts/BlueprintScripts/projectile.cs
+++ reverse rpg/Assets/JustinFolder/Scripts/BlueprintScripts/projectile.cs
@@ -6,6 +6,17 @@ public class projectile : MonoBehaviour
     public float lifeTime;
     public LayerMask enemyLayer;
     public float damage;
+    public AudioClip impactSound;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
 
     void Update()
     {
@@ -20,6 +31,8 @@ public class projectile : MonoBehaviour
 
     public virtual void OnCollisionEnter(Collision collision)
     {
+        bool hitFloor = collision.gameObject.CompareTag("Floor") || collision.gameObject.CompareTag("Ground");
+
         if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
             HealthSystem healthSystem = collision.gameObject.GetComponent<HealthSystem>();
@@ -29,6 +42,14 @@ public class projectile : MonoBehaviour
                 healthSystem.TakeDamage(damage);
                 Debug.Log("Hit " + collision.gameObject.name + " for " + damage + " damage");
             }
+        }
+        else if (hitFloor)
+        {
+            if (impactSound != null && audioSource != null)
+            {
+                audioSource.PlayOneShot(impactSound);
+            }
+            Debug.Log("Magic projectile hit the floor");
         }
     }
 }
